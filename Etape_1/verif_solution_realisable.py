bb5609cb5bb2cfc4b3237f93814d5240ae65a4ef
M. Dandin Brieuc <dandin@insa-08135>
2019-04-16 15:25:24 +0200

    Started verification of a solution's feasability

---
+++ Etape_1/verif_solution_realisable.py
@@ -0,0 +1,32 @@
+import sys
+
+def verif_sol_realisable(arg):
+    route = [som1, som2,...]
+
+    #iteration sur le temps
+    while (not fini) or irrealisable:
+        time += 1
+        for nodes:
+            if (departure_node &time == node.start_date):
+                add node to nodes_to_evacuate
+            else:
+
+        for nodes_to_evacuate:
+            # MaJ nmb personnes dans node & arcs sortants
+        for arc in arcs:
+            test(arc.capacity < nmb_pers)
+            if(date_limite < time):
+                irrealisable
+
+        toutes_nodes_vides = 1
+        for(nodes):
+            if(nmb_pers == 0):
+                remove from nodes_to_evacuate
+            else:
+                toutes_nodes_vides
+        if(toutes_nodes_vides == 1):
+            fini = 1
+
+
+
+    processing_time = time
